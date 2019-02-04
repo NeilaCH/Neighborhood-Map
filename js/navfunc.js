dd0678bbe1674efd170f7b397bfe6513153723da
Neila Chettaoui <chettaoui.neila@gmail.com>
2019-02-04 19:15:44 +0100

    Create navfunc.js

---
+++ js/navfunc.js
@@ -0,0 +1,10 @@
+// Source -- w3schools "https://www.w3schools.com/howto/howto_js_collapse_sidebar.asp" 
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
